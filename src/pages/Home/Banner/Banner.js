5609b02e2c021447281f5484f66d6cf891799ea6
sazzadhossainapple <sazzadapple81@gmail.com>
2022-12-15 16:18:28 +0600

    updated file

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -5,16 +5,12 @@ const Banner = () => {
   return (
     <section className="banner__container">
       <div className="banner__container-text">
-        <div
-          className="banner__img"
-          style={{
-            backgroundImage: `url(${IMG})`,
-          }}
-        >
-          <div className="banner__text">
-            <h2>INNOVATION DIGITAL TECHNOLOGY</h2>
-            <p>everything we do is the consumer, imagination and you.</p>
-          </div>
+        <div className="banner__text">
+          <h2>INNOVATION DIGITAL TECHNOLOGY</h2>
+          <p>everything we do is the consumer, imagination and you.</p>
+        </div>
+        <div className="banner-images">
+          <img src={IMG} alt="" />
         </div>
       </div>
     </section>
